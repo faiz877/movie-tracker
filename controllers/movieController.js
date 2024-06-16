15bd38783572cf19166a897b40d56262663a3607
faiz877 <faiz74787@gmail.com>
2024-06-16 12:31:03 +0530

    feat : updated crud operations

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,16 @@
-const createMovie = (req, res) => {
-  res.send("Create Movie");
+const Movie = require("../models/Movie");
+const { StatusCodes } = require("http-status-codes");
+
+const createMovie = async (req, res) => {
+  const movie = await Movie.create(req.body);
+  res.status(StatusCodes.CREATED).json({ movie });
+};
+const getAllMovies = async (req, res) => {
+  const movie = await Movie.find();
+  res.status(StatusCodes.OK).json({ movie });
 };
 const getMovie = (req, res) => {
   res.send("Get Movie");
-};
-const getAllMovies = (req, res) => {
-  res.send("Get All Movies");
 };
 const updateMovie = (req, res) => {
   res.send("Update Movie");
